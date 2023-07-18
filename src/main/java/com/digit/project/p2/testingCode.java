49db14530aa65b2d742408420a22655395cd1243
vipulGoDigit <139847654+vipulGoDigit@users.noreply.github.com>
2023-07-18 12:43:19 +0530

    Update testingCode.java

--- src/main/java/com/digit/project/p2/testingCode.java
+++ src/main/java/com/digit/project/p2/testingCode.java
@@ -3,7 +3,7 @@ package com.digit.project.p2;
 public class testingCode {
 	 public static void main(String[] args) {
 		System.out.println("Hi , I am VIPUL ");
-		System.out.println("Hi , I am VIPUL ");
+		
 		
 		
 		
