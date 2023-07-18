8062ed7abac948f5bea10170da444ab094215141
Vipul.1 <Vipul.1@LDIGITBGL-L0217.godigit.corp>
2023-07-18 12:26:04 +0530

    This is my First Commit

---
+++ src/main/java/com/digit/project/p2/testingCode.java
@@ -0,0 +1,10 @@
+package com.digit.project.p2;
+
+public class testingCode {
+	 public static void main(String[] args) {
+		System.out.println("Hi , I am VIPUL ");
+		System.out.println("Hi , I am VIPUL ");
+		System.out.println("Hi , I am VIPUL ");
+		System.out.println("Hi , I am VIPUL ");
+	}
+}
